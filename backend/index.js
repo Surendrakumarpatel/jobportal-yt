77981e869c0a0065899af3b5b9a47004717bd464
mohankoli02 <145965105+mohankoli02@users.noreply.github.com>
2024-08-26 23:10:27 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin: "https://job-portal-website-frontend-0nx0.onrender.com",
     credentials:true
 }
 
