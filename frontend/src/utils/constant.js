240d9dd66af460f2afbeff6b91dca29cbeb35adf
Ubuntu <ubuntu@ip-10-0-2-157.ec2.internal>
2024-12-19 18:32:37 +0000

    changes for deployment

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,7 +1,7 @@
-export const USER_API_END_POINT="http://localhost:3000/api/v1/user";
-export const JOB_API_END_POINT="http://localhost:3000/api/v1/job";
-export const APPLICATION_API_END_POINT="http://localhost:3000/api/v1/application";
-export const COMPANY_API_END_POINT="http://localhost:3000/api/v1/company";
+export const USER_API_END_POINT="/api/v1/user";
+export const JOB_API_END_POINT="/api/v1/job";
+export const APPLICATION_API_END_POINT="/api/v1/application";
+export const COMPANY_API_END_POINT="/api/v1/company";
 
 export const Job_Locations={
     "states": [
